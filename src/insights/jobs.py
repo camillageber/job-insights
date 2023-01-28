3c730830f333adcfd6eabdac3d7265a223060271
camillageber <camilla.geber@gmail.com>
2023-01-28 01:49:06 -0300

    chore: implemented function 'filter_by_job_type'

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -59,4 +59,6 @@ def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
     list
         List of jobs with provided job_type
     """
-    raise NotImplementedError
+
+    filter = [job for job in jobs if job["job_type"] == job_type]
+    return filter
