8ed278bfd4571f877898799a91e640f08b0d6754
camillageber <camilla.geber@gmail.com>
2023-01-28 01:51:51 -0300

    chore: implemented function 'filter_by_industry'

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -37,4 +37,5 @@ def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
     list
         List of jobs with provided industry
     """
-    raise NotImplementedError
+    filter = [job for job in jobs if job["industry"] == industry]
+    return filter
