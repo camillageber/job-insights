17aa6fc9f9053539df17b067faa5ec6de6b38062
camillageber <camilla.geber@gmail.com>
2023-01-28 17:03:39 -0300

    test: implemented function 'test_counter'

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,10 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+    low_case = "javascript"
+    upper_case = "JAVASCRIPT"
+
+    assert count_ocurrences(path, low_case) == 122
+    assert count_ocurrences(path, upper_case) == 122
